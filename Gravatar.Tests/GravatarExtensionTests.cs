046c61904ce2c741e0a3fe230352ab94ed20f93f
Andre Baltieri <andrebaltieri@hotmail.com>
2021-03-02 14:13:10 -0300

    Implementado suporte a tamanho da imagem

--- Gravatar.Tests/GravatarExtensionTests.cs
+++ Gravatar.Tests/GravatarExtensionTests.cs
@@ -7,16 +7,18 @@ namespace Gravatar.Tests
     {
         [TestCategory("Gravatar Tests")]
         [TestMethod("Should validate Gravatar extension")]
-        [DataRow(null, false)]
-        [DataRow("", false)]
-        [DataRow(" ", false)]
-        [DataRow("a@a", false)]
-        [DataRow("a@andre.com", false)]
-        [DataRow("andre@balta.io", true)]
-        public void ShouldValidateGravatar(string email, bool status)
+        [DataRow(null, 200, false)]
+        [DataRow("", 200, false)]
+        [DataRow(" ", 200, false)]
+        [DataRow("a@a", 200, false)]
+        [DataRow("a@andre.com", 200, false)]
+        [DataRow("andre@balta.io", null, true)]
+        [DataRow("andre@balta.io", 200, true)]
+        public void ShouldValidateGravatar(string email, int? size, bool status)
         {
-            var result = "https://www.gravatar.com/avatar/8d9f6b0ffc9150878f1ae9e3ae9bfb07";
-            Assert.AreEqual((email.ToGravatar() == result), status);
+            var imageSize = size.HasValue ? size.Value.ToString() : "80";
+            var result = $"https://www.gravatar.com/avatar/8d9f6b0ffc9150878f1ae9e3ae9bfb07?s={imageSize}";
+            Assert.AreEqual((email.ToGravatar(size ?? 80) == result), status);
         }
     }
 }
